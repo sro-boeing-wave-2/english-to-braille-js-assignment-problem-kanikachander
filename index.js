93d740e54f77f1cb77870eafaf6b53c972742b86
Kanika <kanikachander1@gmail.com>
2018-07-10 17:43:36 +0530

    english to braille js

--- index.js
+++ index.js
@@ -5,3 +5,19 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+const myMap = new Map(englishToBrailleLiteralSet);
+
+function convertTextToBraille() {
+  const inputText = document.getElementById('sourceLangText').value;
+  const inputArray = inputText.split('');
+  let convertedString = '';
+  for (let i = 0; i < inputText.length; i += 1) {
+    convertedString += myMap.get(inputArray[i]);
+  }
+  document.getElementById('targetLangText').innerHTML = convertedString;
+  return convertedString;
+}
+
+document.getElementById('btnConvertEnglishToBraille').addEventListener('click', convertTextToBraille);
